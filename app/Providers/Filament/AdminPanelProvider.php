52fd6ad2fab1015e611e2e7bbe89efb2238c8a65
estiven-18 <nelsoncuartas21@gmail.com>
2026-04-17 17:52:06 -0500

    cambiar el color primario del panel de administración

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -29,7 +29,8 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->login()
             ->colors([
-                'primary' => Color::Amber,
+                //here you can customize system colors or add new ones
+                'primary' => Color::Green,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
