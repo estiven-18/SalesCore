78cd8c2f8ede3755cc33b76da2e04a7fe97c8910
estiven-18 <nelsoncuartas21@gmail.com>
2026-04-19 19:09:50 -0500

    se añadio stock_security a productc para saber cuando mandar la alerta

--- database/migrations/2026_04_13_170428_create_products_table.php
+++ database/migrations/2026_04_13_170428_create_products_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->text('description')->nullable();
             $table->decimal('price', 10, 2);
             $table->integer('stock')->default(0);
+            $table->integer('stock_security')->default(0);
             $table->decimal('tax_rate', 5, 2)->default(0);
             $table->boolean('active')->default(true);
             $table->softDeletes();
